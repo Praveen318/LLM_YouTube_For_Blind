6f7ca75e656bdc4fe58be940f1caf914143227bf
Praveen <praveenkrsingh0309@gmail.com>
2023-12-17 15:50:57 +0530

    Front-end

--- LoadingModel_Deployement_Scoring_Ranking/sample_rest_server.py
+++ LoadingModel_Deployement_Scoring_Ranking/sample_rest_server.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 import search_and_rerank
+from pydantic import BaseModel
+
+# app = FastAPI()
+
+# @app.get("/rerank/")
+
+# def rerank(query:str = "testvalue",count: int = 5):
+#     return JSONResponse(content=search_and_rerank.rerank_for_query(query, count))
+
+
 
 app = FastAPI()
 
-@app.get("/rerank/")
+class Item(BaseModel):
+    query: str
+    count: int
 
-def rerank(query:str = "testvalue",count: int = 5):
-    return JSONResponse(content=search_and_rerank.rerank_for_query(query, count))
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/rerank/")
+def rerank(item: Item):
+    return JSONResponse(content=search_and_rerank.rerank_for_query(item.query, item.count))
